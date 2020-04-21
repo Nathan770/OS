4cddaa767a6b5d24ce5a6113d85890d11eca0be5
Amit <amitamihud@gmail.com>
2020-04-21 09:42:48 +0300

    ds

---
+++ v2_parent.c
@@ -0,0 +1,5 @@
+//
+// Created by me on 20/04/2020.
+//
+
+#include "v2_parent.h"
---
+++ v2_parent.h
@@ -0,0 +1,8 @@
+//
+// Created by me on 20/04/2020.
+//
+
+#ifndef EXE_1_V2_PARENT_H
+#define EXE_1_V2_PARENT_H
+
+#endif //EXE_1_V2_PARENT_H
